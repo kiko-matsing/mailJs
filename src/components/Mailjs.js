baf1edd9536c52f0dbb1785d4eff744b7dbc70ac
Francisco G. Quinola <francisco.quinola@legalmatch.com>
2024-10-09 14:39:28 +0800

    Add Api call|

--- src/components/Mailjs.js
+++ src/components/Mailjs.js
@@ -1,28 +1,61 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 const Mailjs = () => {
-  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  const sendEmail = (e) => {
+  const handleSubmit = (e) => {
+
     e.preventDefault();
+    const serviceId = "service_k1l9heg"
+    const templateId = "template_l4v41nt"
+    const publicKey = "y77VhhP3PrgRq_WAP"
+  
 
-    emailjs
-      .sendForm('service_k1l9heg', 'template_l4v41nt', form.current, {
-        publicKey: 'y77VhhP3PrgRq_WAP',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-  };
+  const templateParams = {
+    from_name: name,
+    from_email: email,
+    to_name: 'Kiko',
+    message: message
+
+    }
+
+  emailjs.send(serviceId, templateId, templateParams, publicKey)
+    .then((response) => {
+      console.log("Email sent succesfully", response)
+      setName('')
+      setEmail('')
+      setMessage('')
+    })
+    .catch((error) => {
+      console.error('Error Sending Message', error)
+    })
+
+
+  }
+  // const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs
+  //     .sendForm('service_k1l9heg', 'template_l4v41nt', form.current, {
+  //       publicKey: 'y77VhhP3PrgRq_WAP',
+  //     })
+  //     .then(
+  //       () => {
+  //         console.log('SUCCESS!');
+  //       },
+  //       (error) => {
+  //         console.log('FAILED...', error.text);
+  //       },
+  //     );
+  // };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
+    <form onSubmit={handleSubmit}>
       <label>Name</label>
       <input type="text" name="from_name" />
       <label>Email</label>
